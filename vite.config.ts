e504c48f5552f5c9b82a478f2bf2918b32e20818
sabinapopescu <sabina.popescu@isa.utm.md>
2025-10-22 16:39:29 +0300

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 8080,
   },
   plugins: [react()],
-  base: '/maya_pearl/',
+  base: '/TMPSlab/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
